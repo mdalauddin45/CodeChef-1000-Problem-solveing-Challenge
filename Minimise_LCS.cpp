e67fc5218f4cdb23d3a007153066888e89cd5ea0
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2023-09-22 00:05:10 +0600

    Minimise_lcs case error

---
+++ Minimise_LCS.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        string a;
+        cin>>a;
+        string b;
+        cin>>b;
+        reverse(b.begin(),b.end());
+        int m = b.size();
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(j==0 || i==0) dp[i][j]=0;
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(a[i-1]==b[j-1]){
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        int lcs=dp[n][m];
+        cout<<lcs<<endl;
+    }
+    
+    return 0;
+}
---
+++ a.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        string a;
+        cin>>a;
+        string b;
+        cin>>b;
+        // string b=a;
+        // reverse(b.begin(),b.end());
+        cout<<a<<endl;
+        cout<<b<<endl;
+    }
+    
+    return 0;
+}
